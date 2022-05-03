95876639bfa1a72b8e530c3849345c1f5f43ff60
mostafaaa <mostafao@moe.edu.kw>
2022-05-03 17:33:36 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ecommerce_product.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
